f40e417bcd375605426c148f663db5ff84c7e130
Kevin Schoen <kevschoen83@gmail.com>
2022-09-12 13:53:07 -0500

    Adding react code

--- QuickReferenceREST/quick-reference-rest/quick-reference-rest/src/main/java/com/genspark/quickreferencerest/Controller/ReferenceController.java
+++ QuickReferenceREST/quick-reference-rest/quick-reference-rest/src/main/java/com/genspark/quickreferencerest/Controller/ReferenceController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ReferenceController {
 
     @Autowired
