2dda21dc750c0c37fedec785de1714338a0e08a8
Kevin Schoen <kevschoen83@gmail.com>
2022-09-16 08:40:24 -0500

    Frontend - data table
    Connected the backend and created a table that uploads data from the database.

--- QuickReferenceREST/quick-reference-rest/quick-reference-rest/src/main/java/com/genspark/quickreferencerest/Controller/ReferenceController.java
+++ QuickReferenceREST/quick-reference-rest/quick-reference-rest/src/main/java/com/genspark/quickreferencerest/Controller/ReferenceController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ReferenceController {
 
     @Autowired
